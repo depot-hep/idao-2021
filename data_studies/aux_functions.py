84f1a2e9c26690deddf726764b55f38915c444e1
yaourtpourtoi <oleg.filatov@phystech.edu>
2021-03-31 13:05:15 +0200

    calibration curve update

--- data_studies/aux_functions.py
+++ data_studies/aux_functions.py
@@ -10,9 +10,22 @@ def invert_regr(a, b, y):
     return E_regr
 
 def fit_line(df):
-    X_NR = df.query('event_energy>3 and event_energy<30 and event_class == "NR"').sig_count_y.values.reshape(-1, 1).ravel()
-    y_NR = df.query('event_energy>3 and event_energy<30 and event_class == "NR"').event_energy.values.reshape(-1, 1).ravel()
-    a_NR, b_NR = np.polyfit(y_NR, X_NR, deg=1)
+    X_NR = df.query('event_energy>=3 and event_energy<=30 and event_class == "NR"').event_energy
+    y_NR = df.query('event_energy>=3 and event_energy<=30 and event_class == "NR"').sig_count_y
+    a_NR, b_NR = np.polyfit(X_NR, y_NR, deg=1)
+    #
+    xmin, xmax = 0, 40
+    fig = plt.figure(figsize=(15,12))
+    plt.scatter(X_NR, y_NR, label='true', alpha=0.3)
+    plt.plot([xmin, xmax], a_NR*np.array([xmin, xmax])+b_NR, label='predicted')
+    plt.xlim(xmin, xmax)
+    # plt.ylim(-1000, 40000)
+    plt.xlabel('true energy')
+    plt.ylabel('sig_count_y')
+    plt.legend(loc='upper center')
+    plt.title(f'NR E<->sig_count_y calibration curve, a: {a_NR}, b: {b_NR}')
+    plt.show()
+    fig.savefig('calibration_curve_NR.png')
     return a_NR, b_NR
 
 class Sigma_classifier:
